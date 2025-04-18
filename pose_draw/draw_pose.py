fc393049bcfb4e509dc23eb7d73a2c71c951b13f
teal024 <teal024@foxmail.com>
2025-04-18 04:21:09 +0000

    tweak drawing

--- pose_draw/draw_pose.py
+++ pose_draw/draw_pose.py
@@ -212,25 +212,31 @@ def draw_pose_to_canvas(poses, pool, H, W, reshape_flag, points_only_flag):
         canvas_lst.append(canvas_img)
     return canvas_lst
 
-def convert_3dpose_to_2dpose_body(body_keypoints):
+def convert_3dpose_to_2dpose_body(body_keypoints, face_keypoints):
     """
     将20点的3D坐标映射到18点的2D坐标。
     :param poses: 输入的20点坐标列表，每个点为 [x, y, z]
     :return: 映射得到的18点坐标列表，每个点为 [x, y]
     """
     # 映射关系：索引位置
-    mapping = {
+    body_mapping = {
         0: 1, 1: 2, 2: 3, 3: 4, 4: 5, 5: 6, 6: 7, 8: 8, 9: 9, 10: 10,
         14: 11, 15: 12, 16: 13, 11: 18, 12: 19, 13: 20, 17: 21, 18: 22, 19: 23
+    }
+    face_mapping = {
+        1: 16, 8: 14, 4: 0, 7: 15, 0: 17
     }
     
     # 初始化18点坐标列表，默认值为 [-1, -1]
     result = [[-1, -1] for _ in range(24)]
     
     # 遍历映射关系，将对应的20点坐标映射到18点坐标
-    for src_idx, dst_idx in mapping.items():
+    for src_idx, dst_idx in body_mapping.items():
         if src_idx < len(body_keypoints):  # 确保索引不越界
             result[dst_idx] = [body_keypoints[src_idx][1],body_keypoints[src_idx][0]]   # 提取 x, y 坐标
+    for src_idx, dst_idx in face_mapping.items():
+        if src_idx < len(face_keypoints):
+            result[dst_idx] = [face_keypoints[src_idx][1], face_keypoints[src_idx][0]]
     
     return result
 
@@ -270,7 +276,7 @@ def convert_3dpose_to_2dpose_hand(left_hand_keypoints, right_hand_keypoints, bod
     return [left_result, right_result]
 
 def convert_3dpose_to_2dpose_face(face_keypoints):
-    result = [[pt[1], pt[0]] for pt in face_keypoints]
+    result = [[-1, -1] if i in [0, 1, 4, 5, 6, 7, 8] else [pt[1], pt[0]] for i, pt in enumerate(face_keypoints)]
     return result
 
 
@@ -280,9 +286,9 @@ def read_jsonl(jsonl_path):
     poses = []
     with jsonlines.open(jsonl_path) as reader:
         for obj in reader:
-            condidate = convert_3dpose_to_2dpose_body(obj["body"])
-            subset = [[ -1,  1.,  2.,  3.,  4.,  5.,  6.,  7.,  8.,  9., 10., 11., 12.,
-        13., -1, -1, -1, -1, 18, 19, 20, 21, 22, 23]]
+            condidate = convert_3dpose_to_2dpose_body(obj["body"], obj["face"])
+            subset = [[ 0.,  1.,  2.,  3.,  4.,  5.,  6.,  7.,  8.,  9., 10., 11., 12.,
+        13., 14., 15., 16., 17., 18., 19., 20., 21., 22., 23.]]
             faces = [convert_3dpose_to_2dpose_face(obj["face"])]
             hands = convert_3dpose_to_2dpose_hand(obj["left_hand"],obj["right_hand"], obj["body"])
             poses.append({"bodies":{"candidate": condidate, "subset": subset}, "faces": faces, "hands": hands})
